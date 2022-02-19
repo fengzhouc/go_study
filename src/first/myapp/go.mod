c629964c773872d0a85877a3334e9b091d913a9f
alummox <fengzhouc@foxmail.com>
2022-02-19 09:34:19 +0800

    go first

---
+++ src/first/myapp/go.mod
@@ -0,0 +1,6 @@
+module myapp
+
+go 1.17
+
+// require mymain v0.0.0
+// replace mymain => ../mymain
---
+++ src/first/myapp/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"myapp/mymath"
+	// "mymain"
+)
+
+func main() {
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymath.Sqrt(2))
+	// fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymain.Sqrt(2))
+}
